a01e30ddfc485ab81e4e03aad9b64267256e0b34
Jordan Lewis <jordankareemlewis@gmail.com>
2020-10-10 21:30:49 -0500

    added functions to place marks and switch turns

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,29 @@
-window.onload=function(){
+window.onload=function boardlayout(){
 var grid= document.querySelectorAll('#board div');
 for(let x=0;x<grid.length;x++){
     grid[x].classList.add('square');
+    grid[x].addEventListener('click',playturn,{once: true});
 }
 }
-
+let xturn=true;
+function playturn(e){
+    addMark(e.target,xturn);
+    switchturn();
+    console.log("Square "+" clicked");
+}
+function boardmoves(){
+    let moves=[[],[],[],[],[],[],[],[],[]];
+    
+}
+function addMark(cell,xturn){
+    if (xturn){
+        cell.textContent="X";
+        cell.classList.add("X")
+    }else{
+        cell.textContent="O";
+        cell.classList.add("O")
+    }
+}
+function switchturn(){
+    xturn=!xturn;
+}
