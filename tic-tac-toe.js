4c79b8e594e018472e18b9b08384e0d3b381f34c
Jordan Lewis <jordankareemlewis@gmail.com>
2020-10-11 12:50:17 -0500

    added functionality for hovering and changing status display

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,15 +1,19 @@
+
 window.onload=function boardlayout(){
-var grid= document.querySelectorAll('#board div');
+    var grid= document.querySelectorAll('#board div');
 for(let x=0;x<grid.length;x++){
     grid[x].classList.add('square');
+    setHover(grid[x]);
     grid[x].addEventListener('click',playturn,{once: true});
+    
 }
 }
 let xturn=true;
 function playturn(e){
     addMark(e.target,xturn);
     switchturn();
-    console.log("Square "+" clicked");
+    setStatus();
+    console.log("Square clicked");
 }
 function boardmoves(){
     let moves=[[],[],[],[],[],[],[],[],[]];
@@ -27,3 +31,30 @@ function addMark(cell,xturn){
 function switchturn(){
     xturn=!xturn;
 }
+
+function setStatus(){
+    let statusbox=document.querySelector('#status');
+    if (xturn){
+        statusbox.innerHTML="It's currently X's Turn";
+    }else{
+        statusbox.textContent="It's currently O's Turn"; 
+    }
+}
+function setHover(cell){
+    cell.onmouseover=function(){
+        if (cell.textContext==null){
+            cell.classList.add("hover");
+        }
+    }
+    cell.onmouseout=function(){
+        cell.className="square";
+        if (cell.textContent=="X"){
+            cell.classList.add("X")
+        }
+        if (cell.textContent=="O"){
+            cell.classList.add("O")
+        }
+}
+}
+
+
